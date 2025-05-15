15bde66a8d95f16a52e32bb2672585711aa87cb0
Lypsil0n <weeqoo789@gmail.com>
2025-05-15 08:29:41 +0200

    Add docstring for dao functions

--- db/dao.py
+++ db/dao.py
@@ -5,13 +5,16 @@ class DAO:
         self.conn_string = "dbname='crimedb' user='dbadm' password='dbadm' host='localhost' port='5432'"
 
     def connect(self):
+        "Connect to the database"
         conn = p.connect(self.conn_string)
         return conn
     
     def close(self, conn):
+        "Close the connection to the database"
         conn.close()
     
     def get_by_query(self, conn, query):
+        "Fetch records based on query"
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM mal where malnummer like '%{query}%' OR tilltalad like '%{query}%' OR personnummer like '%{query}%' OR brottsrubricering like '%{query}%'")
         res = cursor.fetchall()
@@ -20,6 +23,7 @@ class DAO:
         return res
     
     def get_all(self, conn):
+        "Fetch all records"
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM mal")
         res = cursor.fetchall()
